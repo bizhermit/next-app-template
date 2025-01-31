00bd35d97372e1803fddc3e22cff7816e5051cf3
bizhermit <rsenda@bizhermit.com>
2025-01-31 16:04:40 +0900

    fix: signout redirect no use next-auth redirect case not qual origin

--- src/app/(nav)/layout-client.tsx
+++ src/app/(nav)/layout-client.tsx
@@ -2,18 +2,21 @@
 
 import { useLang } from "@/i18n/react-hook";
 import { Button } from "@/react/elements/button";
+import useRouter from "@/react/hooks/router";
 import { signOut } from "next-auth/react";
 import { signInPageUrl } from "~/auth/consts";
 
 export const SignOutButton = () => {
   const lang = useLang();
+  const router = useRouter();
 
   return (
     <Button
       onClick={async () => {
         await signOut({
-          callbackUrl: signInPageUrl,
+          redirect: false,
         });
+        router.push(signInPageUrl);
       }}
     >
       {lang("auth.signOutBtn")}
